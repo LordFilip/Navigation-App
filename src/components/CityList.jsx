d0e0b9f20b9bc059a430fda7fd3641e67242bded
Filip <filipivanovic59@gmail.com>
2024-09-17 18:50:35 +0200

    Implementing Context API

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,24 +1,26 @@
 import CityItem from "./CityItem";
-import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
+import styles from "./CityList.module.css";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-export default function CityList({ cities, isLoading }) {
-  if (isLoading) {
-    return <Spinner />;
-  }
+function CityList() {
+  const { cities, isLoading } = useCities();
+  if (isLoading) return <Spinner />;
 
-  if (!cities.length) {
+  if (!cities.length)
     return (
-      <Message message="Add your first city by clickin on the city on the map." />
+      <Message message="Add your first city by clicking on a city on the map" />
     );
-  }
 
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
-        <CityItem city={city} key={city.id} />
+        <CityItem key={city.id} city={city} />
       ))}
     </ul>
+    // <div className={styles.CityList}>City</div>
   );
 }
+
+export default CityList;
