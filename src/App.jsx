bc0bec87a4e12b19b80c10943fb326d0a97f1bda
Filip <filipivanovic59@gmail.com>
2024-09-17 11:52:56 +0200

    Nested Routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,15 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<li>list of cities</li>} />
+          <Route path="cities" element={<li>List of cities</li>} />
+          <Route path="countries" element={<li>List of countries</li>} />
+          <Route path="form" element={<li>FORM</li>} />
+        </Route>
         <Route path="login" element={<Login />} />
 
         <Route path="*" element={<PageNotFOund />} />
--- src/pages/AppNav.jsx
+++
@@ -1,3 +0,0 @@
-export default function AppNav() {
-  return <nav>app nav</nav>;
-}
