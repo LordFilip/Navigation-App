b90447682436bafe482b2a1de5da07aea7575421
Filip <filipivanovic59@gmail.com>
2024-09-18 14:41:44 +0200

    Fetching City Data in the Form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -14,25 +14,25 @@ import {
   useMapEvents,
 } from "react-leaflet";
 import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 export default function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
+
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
 
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(() => {
     if (mapLat && mapLng) {
       setMapPosition([parseFloat(mapLat), parseFloat(mapLng)]);
     }
-  }, [searchParams]);
+  }, [mapLat, mapLng]);
 
   useEffect(
     function () {
